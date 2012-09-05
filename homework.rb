098850b0ceb37ac2f933358ff3daf1be8c419eee
Sergey Krivko <kriv4ik@rambler.ru>
2012-09-05 22:34:01 +0300

    my homework #1

---
+++ homework.rb
@@ -0,0 +1,56 @@
+# homework number 1 #
+
+#t ask1 #
+ def sum(a, b)
+   puts "#{a} + # {b} = #{a+b}"
+end
+
+
+sum(3, 7)
+#3 + 7 = 10
+#=>nil
+
+
+
+
+# task 2 #
+
+
+char_count("aabbbccceABC") #=> {"a" => 3, "b" => 4, "c" => 4, "e" => 1}
+
+
+
+
+char_count ("PriveeETTTvOOOVaaaA") #=> {"a" =>4, "e" =>3, "i" =>1, "p" =>1, "r" =>1 , "t" =>3,  "o" =>3}
+
+
+
+#task 3#
+
+
+	
+fib(7) #=> [4, 8, 12, 20, 32, 52, 84]
+
+
+#task 4#
+
+
+square_sum([3,2,6]) #=> 49
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
